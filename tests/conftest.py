c2310b9a90c0f1b2bd38ecf5065c56e1b9840808
Sasha <q.dev.null@gmail.com>
2020-10-25 08:35:04 +0100

    Add the tests

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+import numpy as np
+from models.baseline import read_data
+
+
+@pytest.fixture
+def xy():
+    train = read_data("data/train_features.csv")
+    targets = read_data("data/train_targets_scored.csv")
+    for i, _ in enumerate(targets.columns):
+        targets.iloc[:, i] = np.random.binomial(2, 0.5, targets.shape[0])
+    return train.head(100), targets.head(100)
