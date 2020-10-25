13928bd9a55868aa22dc984d04455b7f46bb098b
Sasha <q.dev.null@gmail.com>
2020-10-25 08:21:50 +0100

    Add the simple baseline

---
+++ models/baseline.py
@@ -0,0 +1,105 @@
+# Inspired by:
+# https://www.kaggle.com/fchmiel/xgboost-baseline-multilabel-classification
+
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import KFold
+from category_encoders import CountEncoder
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import log_loss
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.linear_model import SGDClassifier
+
+
+SEED = 42
+NFOLDS = 5
+DATA_DIR = "/kaggle/input/lish-moa/"
+np.random.seed(SEED)
+
+
+def build_model():
+    # Former xgboost parameters
+    # params = {
+    #     "tree_method": "gpu_hist",
+    #     "colsample_bytree": 0.6522,
+    #     "gamma": 3.6975,
+    #     "learning_rate": 0.0503,
+    #     "max_delta_step": 2.0706,
+    #     "max_depth": 10,
+    #     "min_child_weight": 31.5800,
+    #     "n_estimators": 166,
+    #     "subsample": 0.8639
+    # }
+
+    model = make_pipeline(
+        CountEncoder(cols=[0, 2]),
+        MultiOutputClassifier(SGDClassifier()),
+    )
+
+    return model
+
+
+def read_data(path, ignore_col="sig_id"):
+    df = pd.read_csv(path)
+    df.drop(columns=[ignore_col], inplace=True)
+    return df
+
+
+def main():
+    train = read_data("data/train_features.csv")
+    targets = read_data("data/train_targets_scored.csv")
+
+    test = read_data("data/test_features.csv")
+    sub = read_data("data/sample_submission.csv")
+
+    # drop id col
+    X = train.to_numpy()
+    X_test = test.to_numpy()
+    y = targets.to_numpy()
+
+    clf = build_model()
+
+    oof_preds = np.zeros(y.shape)
+    test_preds = np.zeros((test.shape[0], y.shape[1]))
+    oof_losses = []
+    kf = KFold(n_splits=NFOLDS)
+    for fn, (trn_idx, val_idx) in enumerate(kf.split(X, y)):
+        print("Starting fold: ", fn)
+        X_train, X_val = X[trn_idx], X[val_idx]
+        y_train, y_val = y[trn_idx], y[val_idx]
+
+        # drop where cp_type==ctl_vehicle (baseline)
+        ctl_mask = X_train[:, 0] == "ctl_vehicle"
+        X_train = X_train[~ctl_mask, :]
+        y_train = y_train[~ctl_mask]
+
+        clf.fit(X_train, y_train)
+        val_preds = clf.predict_proba(X_val)  # list of preds per class
+        val_preds = np.array(val_preds)[:, :, 1].T  # take the positive class
+        oof_preds[val_idx] = val_preds
+
+        loss = log_loss(np.ravel(y_val), np.ravel(val_preds))
+        oof_losses.append(loss)
+        preds = clf.predict_proba(X_test)
+        preds = np.array(preds)[:, :, 1].T  # take the positive class
+        test_preds += preds / NFOLDS
+
+    print(oof_losses)
+    print("Mean OOF loss across folds", np.mean(oof_losses))
+    print("STD OOF loss across folds", np.std(oof_losses))
+
+    # set control train preds to 0
+    control_mask = train["cp_type"] == "ctl_vehicle"
+    oof_preds[control_mask] = 0
+
+    print("OOF log loss: ", log_loss(np.ravel(y), np.ravel(oof_preds)))
+
+    # create the submission file
+    sub.iloc[:, 1:] = test_preds
+    sub.to_csv("submission.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
