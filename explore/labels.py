4d52cffa2285c307838220cf5ac3bc2c2bc471b4
Sasha <q.dev.null@gmail.com>
2020-10-31 09:03:47 +0100

    Add the label distribution

--- explore/labels.py
+++ explore/labels.py
@@ -15,17 +15,37 @@ def main():
     y = read_data("data/train_targets_scored.csv", return_df=True)
     coo = y.T @ y
 
+    # Check co-occurrence
+    plt.matshow(plotable(coo.values))
+    plt.xlabel("label number")
+    plt.ylabel("label number")
+    plt.savefig('coo.png')
+    plt.show()
+
     # Check conditional probabilities
     interaction = coo.values.copy() / coo.values.sum(axis=1, keepdims=True)
     np.fill_diagonal(interaction, 0)
     nonzero = ~np.all(interaction == 0, axis=0)
     plt.matshow(plotable(interaction[np.ix_(nonzero, nonzero)]))
+    plt.xlabel("interactiong label number")
+    plt.ylabel("interactiong label number")
+    plt.savefig('proba.png')
     plt.show()
 
-    plt.matshow(plotable(coo.values))
+    # Label frequency
+    counts = y.sum(axis=0)
+    significance = (-counts).argsort()
+    x = np.arange(len(significance))
+    plt.figure(figsize=(12, 6))
+    plt.bar(x, counts[significance])
+    plt.plot(x, x * 0 + 20, '--', color='black')
+    plt.xticks(x, y.columns[significance], rotation=90)
+    plt.ylabel("counts")
+    plt.yscale('log')
+    plt.tight_layout()
+    plt.savefig('counts.png')
     plt.show()
 
-    # plt.yticks(np.arange(coo.shape[0]), y.columns)
 
 if __name__ == '__main__':
     main()
