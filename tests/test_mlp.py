9bf26fd4c3c533a3f8bc5d738299f59fe935bd8b
Sasha <q.dev.null@gmail.com>
2020-10-31 06:49:10 +0100

    Add the tests for the cv-pipeline

--- tests/test_mlp.py
+++ tests/test_mlp.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from models.mlp import build_model, build_preprocessor
+from models.mlp import build_model, build_preprocessor, cv_fit
 
 
 @pytest.mark.skip
@@ -14,7 +14,15 @@ def test_preprocessor(xy):
 
 
 def test_model(xy):
-    X, y = xy
+    X = xy[0].to_numpy()
+    y = xy[1].to_numpy().astype(np.float32)
 
     model = build_model()
-    model.fit(X, y.to_numpy().astype(np.float32))
+    model.fit(X, y)
+
+
+def test_cv_model(xy):
+    X = xy[0].to_numpy()
+    y = xy[1].to_numpy().astype(np.float32)
+
+    cv_fit(build_model(), X, y, X)
