8c77f95ea35cdf9e736a4de423eeec17ead69e66
Sasha <q.dev.null@gmail.com>
2020-11-02 06:43:01 +0100

    Fix the multioutput regression

---
+++ tests/test_multioutput.py
@@ -0,0 +1,11 @@
+import pytest
+from models.baseline import build_model, cros_val_fit
+
+
+@pytest.mark.skip("It should not work")
+def test_model(xy):
+    X, y = xy
+
+    model = build_model()
+    model.fit(X, y)
+    cros_val_fit(model, X, y, X)
