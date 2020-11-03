c3e45e12c724cd22b1c7a515d0aa167fd4167c10
kqf <q.dev.null@gmail.com>
2020-11-03 08:51:32 +0100

    Add the alternative cv (#16)
    * Add the iterative stratification
    
    * Rename the module
    
    * Add iterstat for the local validation
    
    * Add the alternative cv to the MLP pipeline

---
+++ models/iterstatlocal.py
@@ -0,0 +1,113 @@
+# <<<<<<<<<<<<
+# The original code was here:
+# https://raw.githubusercontent.com/trent-b/iterative-stratification/master/iterstrat/ml_stratifiers.py
+
+import numpy as np
+
+from sklearn.utils import check_random_state
+from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import type_of_target
+from sklearn.model_selection._split import _BaseKFold
+
+
+def istratify(labels, r, random_state):
+    n_samples = labels.shape[0]
+    test_folds = np.zeros(n_samples, dtype=int)
+
+    # Calculate the desired number of examples at each subset
+    c_folds = r * n_samples
+
+    # Calculate the desired number of examples of each label at each subset
+    c_folds_labels = np.outer(r, labels.sum(axis=0))
+
+    labels_not_processed_mask = np.ones(n_samples, dtype=bool)
+
+    while np.any(labels_not_processed_mask):
+        num_labels = labels[labels_not_processed_mask].sum(axis=0)
+        if num_labels.sum() == 0:
+            sample_idxs = np.where(labels_not_processed_mask)[0]
+
+            for sample_idx in sample_idxs:
+                fold_idx = np.where(c_folds == c_folds.max())[0]
+
+                if fold_idx.shape[0] > 1:
+                    fold_idx = fold_idx[random_state.choice(fold_idx.shape[0])]
+
+                test_folds[sample_idx] = fold_idx
+                c_folds[fold_idx] -= 1
+
+            break
+
+        label_idx = np.where(
+            num_labels == num_labels[np.nonzero(num_labels)].min())[0]
+        if label_idx.shape[0] > 1:
+            label_idx = label_idx[random_state.choice(label_idx.shape[0])]
+
+        sample_idxs = np.where(np.logical_and(
+            labels[:, label_idx].flatten(), labels_not_processed_mask))[0]
+
+        for sample_idx in sample_idxs:
+            # Find the subset(s) with the largest number of desired examples
+            # for this label, breaking ties by considering the largest number
+            # of desired examples, breaking further ties randomly
+            label_folds = c_folds_labels[:, label_idx]
+            fold_idx = np.where(label_folds == label_folds.max())[0]
+
+            if fold_idx.shape[0] > 1:
+                temp_fold_idx = np.where(
+                    c_folds[fold_idx] == c_folds[fold_idx].max())[0]
+                fold_idx = fold_idx[temp_fold_idx]
+
+                if temp_fold_idx.shape[0] > 1:
+                    fold_idx = fold_idx[random_state.choice(
+                        temp_fold_idx.shape[0])]
+
+            test_folds[sample_idx] = fold_idx
+            labels_not_processed_mask[sample_idx] = False
+
+            # Update desired number of examples
+            c_folds_labels[fold_idx, labels[sample_idx]] -= 1
+            c_folds[fold_idx] -= 1
+
+    return test_folds
+
+
+class MultilabelStratifiedKFold(_BaseKFold):
+    def __init__(self, n_splits=3, shuffle=False, random_state=None):
+        super(MultilabelStratifiedKFold, self).__init__(
+            n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+
+    def _make_test_folds(self, X, y):
+        y = np.asarray(y, dtype=bool)
+        type_of_target_y = type_of_target(y)
+
+        if type_of_target_y != 'multilabel-indicator':
+            msg = 'Supported target type is: multilabel-indicator.'\
+                ' Got {!r} instead.'
+            raise ValueError(msg.format(type_of_target_y))
+
+        num_samples = y.shape[0]
+
+        rng = check_random_state(self.random_state)
+        indices = np.arange(num_samples)
+
+        if self.shuffle:
+            rng.shuffle(indices)
+            y = y[indices]
+
+        r = np.asarray([1 / self.n_splits] * self.n_splits)
+
+        test_folds = istratify(labels=y, r=r, random_state=rng)
+
+        return test_folds[np.argsort(indices)]
+
+    def _iter_test_masks(self, X=None, y=None, groups=None):
+        test_folds = self._make_test_folds(X, y)
+        for i in range(self.n_splits):
+            yield test_folds == i
+
+    def split(self, X, y, groups=None):
+        y = check_array(y, ensure_2d=False, dtype=None)
+        return super(MultilabelStratifiedKFold, self).split(X, y, groups)
+
+# <<<<<<<<<<<<
