f60eb5caae1e23712dce648b60f7bb4ed51249d2
Sasha <q.dev.null@gmail.com>
2020-11-05 07:34:16 +0100

    Add the titles to the records

--- explore/lb.py
+++ explore/lb.py
@@ -8,12 +8,14 @@ def main():
 
     fl = r"\d+\.\d+|nan"
     pattern = (
+        r"([a-zA-Z0-9_\(\) \t]+)\n"
         rf"CV losses train ({fl}) \+\/\- ({fl})\w*\n"
         rf"CV losses valid ({fl}) \+\/\- ({fl})\w*\n"
         rf"LB: ({fl})"
     )
     matches = re.findall(pattern, data)
     df = pd.DataFrame(matches, columns=[
+        "title",
         "train",
         "d train",
         "valid",
