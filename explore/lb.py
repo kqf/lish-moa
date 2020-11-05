d6720713526f3589bf856b9c4f2e25ae21b3000a
Sasha <q.dev.null@gmail.com>
2020-11-05 07:44:48 +0100

    Add the lb plot

--- explore/lb.py
+++ explore/lb.py
@@ -1,11 +1,9 @@
 import re
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def main():
-    with open("results.md") as f:
-        data = f.read()
-
+def parse(data):
     fl = r"\d+\.\d+|nan"
     pattern = (
         r"([a-zA-Z0-9_\(\) \t]+)\n"
@@ -22,7 +20,24 @@ def main():
         "d valid",
         "lb",
     ])
+    for col in df.columns:
+        if col != "title":
+            df[col] = df[col].astype(float)
+    return df
+
+
+def main():
+    with open("results.md") as f:
+        data = f.read()
+
+    df = parse(data)
     print(df)
+
+    plt.errorbar(df["lb"], df["valid"], df["d valid"], fmt=".")
+    plt.xlabel("Leaderboard Score")
+    plt.ylabel("CV score")
+    plt.grid()
+    plt.show()
 
 
 if __name__ == '__main__':
