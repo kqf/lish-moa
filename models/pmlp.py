c8f1e617936ff9656797b1f19ca5a3a32a0750e4
Sasha <q.dev.null@gmail.com>
2020-11-29 16:43:02 +0100

    Move the code to the main method

--- models/pmlp.py
+++ models/pmlp.py
@@ -12,23 +12,14 @@ set_tt_rng(MRG_RandomStreams(42))
 floatX = theano.config.floatX
 filterwarnings('ignore')
 
-print('Running on PyMC3 v{}'.format(pm.__version__))
 
-X, Y = make_moons(noise=0.2, random_state=0, n_samples=1000)
-X = scale(X)
-X = X.astype(floatX)
-Y = Y.astype(floatX)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.5)
-
-def construct_nn(ann_input, ann_output):
-    n_hidden = 5
-
+def construct_nn(X, y, n_hidden=5):
     # Initialize random weights between each layer
-    init_1 = np.random.randn(X.shape[1], n_hidden).astype(floatX)
-    init_2 = np.random.randn(n_hidden, n_hidden).astype(floatX)
-    init_out = np.random.randn(n_hidden).astype(floatX)
+    ifc1 = np.random.randn(X.shape[1], n_hidden).astype(floatX)
+    ifc2 = np.random.randn(n_hidden, n_hidden).astype(floatX)
+    ifc3 = np.random.randn(n_hidden).astype(floatX)
 
-    with pm.Model() as neural_network:
+    with pm.Model() as model:
         """
             Trick: Turn inputs and outputs into shared variables using the data container pm.Data
             It's still the same thing, but we can later change the values of the shared variable
@@ -36,43 +27,47 @@ def construct_nn(ann_input, ann_output):
             Kind-of like a pointer we can redirect.
             For more info, see: http://deeplearning.net/software/theano/library/compile/shared.html
         """  # noqa
-        ann_input = pm.Data('ann_input', X_train)
-        ann_output = pm.Data('ann_output', Y_train)
+        ann_input = pm.Data('ann_input', X)
+        ann_output = pm.Data('ann_output', y)
 
         # Weights from input to hidden layer
-        weights_in_1 = pm.Normal('w_in_1', 0, sigma=1,
-                                 shape=(X.shape[1], n_hidden),
-                                 testval=init_1)
+        fc1 = pm.Normal('fc1', 0, sigma=1, shape=(
+            X.shape[1], n_hidden), testval=ifc1)
 
         # Weights from 1st to 2nd layer
-        weights_1_2 = pm.Normal('w_1_2', 0, sigma=1,
-                                shape=(n_hidden, n_hidden),
-                                testval=init_2)
+        fc2 = pm.Normal('fc2', 0, sigma=1, shape=(
+            n_hidden, n_hidden), testval=ifc2)
 
         # Weights from hidden layer to output
-        weights_2_out = pm.Normal('w_2_out', 0, sigma=1,
-                                  shape=(n_hidden,),
-                                  testval=init_out)
+        fc3 = pm.Normal('fc3', 0, sigma=1, shape=(n_hidden,), testval=ifc3)
 
         # Build neural-network using tanh activation function
-        act_1 = pm.math.tanh(pm.math.dot(ann_input,
-                                         weights_in_1))
-        act_2 = pm.math.tanh(pm.math.dot(act_1,
-                                         weights_1_2))
-        act_out = pm.math.sigmoid(pm.math.dot(act_2,
-                                              weights_2_out))
+        act_1 = pm.math.tanh(pm.math.dot(ann_input, fc1))
+        act_2 = pm.math.tanh(pm.math.dot(act_1, fc2))
+        act_out = pm.math.sigmoid(pm.math.dot(act_2, fc3))
 
         # Binary classification -> Bernoulli likelihood
-        out = pm.Bernoulli('out',
-                           act_out,
-                           observed=ann_output,
-                           # IMPORTANT for minibatches
-                           total_size=Y_train.shape[0]
-                           )
-    return neural_network
+        pm.Bernoulli('out',
+                     act_out,
+                     observed=ann_output,
+                     # IMPORTANT for minibatches
+                     total_size=y.shape[0]
+                     )
+    return model
 
 
-neural_network = construct_nn(X_train, Y_train)
-with neural_network:
-    inference = pm.ADVI()
-    approx = pm.fit(n=30000, method=inference)
+def main():
+    X, Y = make_moons(noise=0.2, random_state=0, n_samples=1000)
+    X = scale(X)
+    X = X.astype(floatX)
+    Y = Y.astype(floatX)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.5)
+
+    model = construct_nn(X_train, Y_train)
+    with model:
+        inference = pm.ADVI()
+        approx = pm.fit(n=30000, method=inference)
+
+
+if __name__ == '__main__':
+    main()
